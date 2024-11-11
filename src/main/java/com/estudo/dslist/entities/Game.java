9c26bfa230e337ae24ddd67c744bc209bb90ff09
felipe.machado.ext <felipe.machado.ext@sodexo.com>
2024-11-11 16:24:00 -0300

    Game seed

--- src/main/java/com/estudo/dslist/entities/Game.java
+++ src/main/java/com/estudo/dslist/entities/Game.java
@@ -20,22 +20,28 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Long score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform,
-                String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms,
+                Long score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -74,12 +80,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatform(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public Long getScore() {
+        return score;
     }
 
     public String getImgUrl() {
